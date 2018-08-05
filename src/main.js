0ce918c808ba330c3f3829295a4bd0f5fa0e4e7c
Alesh Slovak <alesh@roadmunk.com>
2018-08-05 00:19:53 -0400

    clean up

--- src/main.js
+++ src/main.js
@@ -151,26 +151,21 @@ class Player {
   }
 }
 
-let app = playground({
-  width     : 240,
-  height    : 135,
-  smoothing : false,
 
-  create: function() {
-    P8.init(this, {
-      images : [ 'monster1', 'monster2' ],
-    })
+P8.run({
+  images : [ 'monster1', 'monster2' ],
 
+  init : function() {
     this.player1 = new Player(10, 10, 'monster1', P8.BROWN)
     this.player2 = new Player(140, 10, 'monster2', P8.RED)
   },
 
-  step: function(dt) {
+  step : function(dt) {
     this.player1.update(dt)
     this.player2.update(dt)
   },
 
-  keydown: function(event) {
+  keydown : function(event) {
     // player 1 controls
     if (event.key === 'w') this.player1.up()
     if (event.key === 's') this.player1.down()
@@ -182,7 +177,7 @@ let app = playground({
     if (event.key === 'enter') this.player2.endTurn(this.player1)
   },
 
-  render: function() {
+  render : function() {
     P8.cls(P8.BLACK)
     this.player1.draw(this.player2)
     this.player2.draw(this.player1)
